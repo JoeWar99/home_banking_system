f88593abbceb2b9aeee7296e79b2cfb13db22218
lockdowpt <guerra70@live.com.pt>
2019-05-05 10:58:37 +0100

    user

--- user.c
+++ user.c
@@ -6,10 +6,26 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
 #include "utilities.h"
 
 int main(){
 
+    int pid  = getpid();    
+    char string_pid[64];
+    int secure_svr;
+    
+    sprintf(string_pid, "%d", pid);
     
 
+    if ((secure_svr = open("/tmp/secure_svr", O_WRONLY)) == -1) {
+        perror("open: server is not working 404 error");
+        exit(1);
+    }
+
+
+    close(secure_svr);
+    return(0);
+
 }
+
