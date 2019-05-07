e8ccbc57901e6932a97985a99b9ecf4da78cd6b4
GambuzX <mg_mesquita@hotmail.com>
2019-05-07 17:09:58 +0100

    Function to parse request arguments

--- user.c
+++ user.c
@@ -11,11 +11,37 @@
 #include "constants.h"
 #include "types.h"
 
-int valid_request_arguments(int operation, char * req_args, int req_arg_count) {
+int parse_req_args(char * req_args_string, char * req_args[3], int * req_arg_count) {
+
+    const char delim[2] = " ";
+    char *token = strtok(req_args_string, delim);
+    for (*req_arg_count = 0; *req_arg_count <= 3; (*req_arg_count)++) {
+
+        if (token == NULL) break;
+
+        if (*req_arg_count == 3) {
+            printf("Invalid number of request arguments\n");
+            return -1;
+        }
+
+        req_args[*req_arg_count] = (char *) malloc(sizeof(char) * (strlen(token)+1));
+        strcpy(req_args[*req_arg_count], token);
+        token = strtok(NULL, delim);
+    }
+    return 0;
+}
+
+int valid_req_args(int operation, char ** req_args, int req_arg_count) {
+
+    if (operation < 0 || operation > 3) {
+        printf("Invalid operation specified\n");
+        return -1;
+    }
+
     switch(operation) {
         case OP_CREATE_ACCOUNT:
             if (req_arg_count != 3) {
-                fprintf(STDERR_FILENO, "Create account operation requires 3 arguments\n");
+                fprintf(stderr, "Create account operation requires 3 arguments\n");
                 return -1;
             }
 
@@ -24,49 +50,51 @@ int valid_request_arguments(int operation, char * req_args, int req_arg_count) {
             char * pwd = req_args[2];
 
             if (account_id <= 0 || account_id >= MAX_BANK_ACCOUNTS) {
-                fprintf(STDERR_FILENO, "Invalid account ID: %d\n", account_id);
+                fprintf(stderr, "Invalid account ID: %d\n", account_id);
                 return -1;
             }
 
             if (balance <= 0 || balance > MAX_BALANCE) {
-                fprintf(STDERR_FILENO, "Invalid balance value: %d\n", balance);
+                fprintf(stderr, "Invalid balance value: %d\n", balance);
                 return -1;
             }
 
             if (strlen(pwd) < MIN_PASSWORD_LEN || strlen(pwd) > MAX_PASSWORD_LEN) {
-                fprintf(STDERR_FILENO, "Password length must be between %d and %d\n", MIN_PASSWORD_LEN, MAX_PASSWORD_LEN);
-                return -1;
-            }          
-
-            break;
-        case OP_BALANCE:
-            if (req_arg_count != 0) {
-                fprintf(STDERR_FILENO, "Balance check operation requires no arguments\n");
+                fprintf(stderr, "Password length must be between %d and %d\n", MIN_PASSWORD_LEN, MAX_PASSWORD_LEN);
                 return -1;
             }
             break;
+
+        case OP_BALANCE:
+            if (req_arg_count != 0) {
+                fprintf(stderr, "Balance check operation requires no arguments\n");
+                return -1;
+            }
+            break;
+
         case OP_TRANSFER:
             if (req_arg_count != 2) {
-                fprintf(STDERR_FILENO, "Transfer operation requires 2 arguments\n");
+                fprintf(stderr, "Transfer operation requires 2 arguments\n");
                 return -1;
             }
             int dest_account_id = atoi(req_args[0]);
             int amount = atoi(req_args[1]);
             
             if (dest_account_id <= 0 || dest_account_id >= MAX_BANK_ACCOUNTS) {
-                fprintf(STDERR_FILENO, "Invalid account ID: %d\n", dest_account_id);
+                fprintf(stderr, "Invalid account ID: %d\n", dest_account_id);
                 return -1;
             }
 
             if (amount <= 0 || amount > MAX_BALANCE) {
-                fprintf(STDERR_FILENO, "Invalid amount value: %d\n", amount);
+                fprintf(stderr, "Invalid amount value: %d\n", amount);
                 return -1;
             }
 
             break;
+
         case OP_SHUTDOWN:
             if (req_arg_count != 0) {
-                fprintf(STDERR_FILENO, "Shutdown account operation requires no arguments\n");
+                fprintf(stderr, "Shutdown account operation requires no arguments\n");
                 return -1;
             }
             break;
@@ -91,31 +119,16 @@ int main(int argc, char * argv[]){
     //op_delay = atoi(argv[3]);
     operation = atoi(argv[4]);
 
-    if (operation < 0 || operation > 3) {
-        printf("Invalid operation specified\n");
+    char * req_args[3];
+    int req_arg_count;
+
+    if (parse_req_args(argv[5], req_args, &req_arg_count) == -1) {
+        fprintf(stderr, "Error parsing request arguments\n");
         exit(RC_OTHER);
     }
 
-    char * req_args[3];
-    int req_arg_count;
-    const char delim[2] = " ";
-    char *token = strtok(argv[5], delim);
-    for (req_arg_count = 0; req_arg_count <= 3; req_arg_count++) {
-
-        if (token == NULL) break;
-
-        if (req_arg_count == 3) {
-            printf("Invalid number of request arguments\n");
-            exit(RC_OTHER);
-        }
-
-        req_args[req_arg_count] = (char *) malloc(sizeof(char) * (strlen(token)+1));
-        strcpy(req_args[req_arg_count], token);
-        token = strtok(NULL, delim);
-    }
-
-    if(valid_request_arguments(operation, req_args, req_arg_count) == -1) {
-        fprintf(STDERR_FILENO, "Invalid request arguments\n");
+    if(valid_req_args(operation, req_args, req_arg_count) == -1) {
+        fprintf(stderr, "Invalid request arguments\n");
         exit(RC_OTHER);
     }
 
