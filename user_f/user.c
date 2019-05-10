5f3067556705ba48960cc41f94a1fb7cb020d515
lockdowpt <guerra70@live.com.pt>
2019-05-10 18:39:07 +0100

    User timeout working

--- user_f/user.c
+++ user_f/user.c
@@ -7,7 +7,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
-
+#include <signal.h>
 #include "../shared/utilities.h"
 #include "../shared/constants.h"
 #include "../shared/types.h"
@@ -15,6 +15,44 @@
 #include "user_parse.h"
 
 #include "../shared/sope.h"
+
+int user_fifo;
+int secure_svr;
+int open_user_fifo_failed = 1;
+
+void exit_user_process()
+{
+    char secure_fifo_name[strlen(USER_FIFO_PATH_PREFIX) + WIDTH_PID + 1];
+    init_secure_fifo_name(secure_fifo_name, getpid());
+   
+    if (open_user_fifo_failed == 0)
+    {
+        if (close(secure_svr) != 0)
+        {
+            perror("close: error closing down server fifo");
+            exit(RC_OTHER);
+        }
+    }
+
+    if (close(user_fifo) != 0)
+    {
+        perror("close: error closing down user fifo");
+        exit(RC_OTHER);
+    }
+
+    /* Unlink user specific fifo */
+    if (unlink(secure_fifo_name) != 0)
+    {
+        perror("unlink: error unlinking user fifo");
+        exit(RC_OTHER);
+    }
+}
+
+void sigalarm_handler(int signo)
+{
+    exit_user_process();
+    exit(RC_SRV_TIMEOUT);
+}
 
 int main(int argc, char *argv[])
 {
@@ -24,6 +62,12 @@ int main(int argc, char *argv[])
         printf("Usage: %s <account_id> <account_pwd> <delay> <operation> <request_args>\n", argv[0]);
         exit(RC_OTHER);
     }
+
+    struct sigaction alarm_aux;
+    alarm_aux.sa_handler = sigalarm_handler;
+    sigemptyset(&alarm_aux.sa_mask);
+    alarm_aux.sa_flags = 0;
+    sigaction(SIGALRM, &alarm_aux, NULL);
 
     int pid = getpid();
     int account_id, op_delay, operation;
@@ -70,7 +114,7 @@ int main(int argc, char *argv[])
     init_request(&full_request, operation, pid, account_id, pwd, op_delay, req_args);
 
     /* Open server fifo */
-    int secure_svr;
+    //   int secure_svr;
     if ((secure_svr = open(SERVER_FIFO_PATH, O_WRONLY)) == -1)
     {
         perror("open: server is not working 404 error");
@@ -78,10 +122,15 @@ int main(int argc, char *argv[])
     }
 
     /* Write request */
-	if(write_request(secure_svr, &full_request) != 0){
-		fprintf(stderr, "write_request: error writing  request to server\n");
-		exit(RC_OTHER);
-	}
+    if (write_request(secure_svr, &full_request) != 0)
+    {
+        fprintf(stderr, "write_request: error writing  request to server\n");
+        exit(RC_OTHER);
+    }
+
+    int error_due_to_alarm = 0;
+
+    alarm(FIFO_TIMEOUT_SECS);
 
     if (logRequest(STDOUT_FILENO, pid, &full_request) < 0)
     {
@@ -90,25 +139,34 @@ int main(int argc, char *argv[])
     }
 
     /* Open user specific fifo */
-    int user_fifo;
+    ///  int user_fifo;
+
     if ((user_fifo = open(secure_fifo_name, O_RDONLY)) == -1)
     {
         perror("open");
         exit(RC_USR_DOWN);
     }
 
+    open_user_fifo_failed = 0;
+
     /* Read server reply */
     tlv_reply_t request_reply;
-	if(read_reply(user_fifo, &request_reply) != 0){
-		fprintf(stderr, "read_reply: error reading reply from fifo\n");
-		exit(RC_OTHER);
-	}
 
-	if (logReply(STDOUT_FILENO, pid, &request_reply) < 0)
-	{
-		fprintf(stderr, "logRequest: error writing reply to stdout\n");
-		exit(RC_OTHER);
-	}
+    if (read_reply(user_fifo, &request_reply) == 0)
+    {
+        alarm(0);
+    }
+    else
+    {
+        fprintf(stderr, "read_reply: error reading reply from fifo\n");
+        exit(RC_OTHER);
+    }
+
+    if (logReply(STDOUT_FILENO, pid, &request_reply) < 0)
+    {
+        fprintf(stderr, "logRequest: error writing reply to stdout\n");
+        exit(RC_OTHER);
+    }
 
     /* Free alocated memory */
     for (int i = 0; i < req_arg_count; i++)
@@ -116,25 +174,7 @@ int main(int argc, char *argv[])
         free(req_args[i]);
     }
 
-    /* Close server fifo descriptor */
-    if (close(secure_svr) != 0)
-    {
-        perror("close: error closing down server fifo");
-        exit(RC_OTHER);
-    }
-
-    if (close(user_fifo) != 0)
-    {
-        perror("close: error closing down user fifo");
-        exit(RC_OTHER);
-    }
-
-    /* Unlink user specific fifo */
-    if (unlink(secure_fifo_name) != 0)
-    {
-        perror("unlink: error unlinking user fifo");
-        exit(RC_OTHER);
-    }
+    exit_user_process();
 
     return RC_OK;
 }
